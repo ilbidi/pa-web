0b7a3fe835900e2f20627f510c98e6d3637fd7dd
ilbidi <fbidinotto@gmail.com>
2017-08-13 14:50:25 +0200

    First basic view

--- pa_web/__init__.py
+++ pa_web/__init__.py
@@ -5,3 +5,6 @@ from flask import Flask
 app = Flask(__name__, instance_relative_config=True)
 app.config.from_object('config')
 app.config.from_pyfile('config.py')
+
+# Import views, the import is at the end to avoid circular import
+from pa_web import views
