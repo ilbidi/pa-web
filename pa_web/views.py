a2fbd19745542e688698eb71f936a105c6043750
ilbidi <fbidinotto@gmail.com>
2017-08-16 16:29:01 +0200

    First templates

--- pa_web/views.py
+++ pa_web/views.py
@@ -1,3 +1,6 @@
+# imports
+from flask import request, render_template
+
 # views
 from pa_web import app
 
@@ -5,4 +8,20 @@ from pa_web import app
 @app.route('/')
 @app.route('/index')
 def index():
-    return 'Hi'
+    return render_template('index.html')
+
+# About
+@app.route('/about')
+def about():
+    return 'About'
+
+# Passing parameters
+@app.route('/param/<param>')
+def param(param):
+    return param
+
+# Enviroment informations
+@app.route('/envinfo')
+def envinfo():
+    user_agent = request.headers.get('User-Agent')
+    return '<p>User Agent = %s</p>' % user_agent
