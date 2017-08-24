acf77d568442484c0fcbd214f97c30cc7ea455d2
ilbidi <fbidinotto@gmail.com>
2017-08-24 21:28:44 +0200

    Added base bootstrap template

--- pa_web/views.py
+++ pa_web/views.py
@@ -34,3 +34,11 @@ def login():
     if( form.validate_on_submit() ):
         return redirect(url_for('index'))
     return render_template('login.html', form=form)
+
+# Managing redirect errors
+@app.errorhandler(404)
+def page_not_found(e):
+    return render_template('404.html'), 404
+@app.errorhandler(500)
+def internal_server_error(e):
+    return render_template('500.html'), 500
