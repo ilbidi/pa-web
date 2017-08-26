5eabb0c7d8a561839df0d903665226461252de1c
ilbidi <fbidnotto@gmail.com>
2017-08-26 17:02:07 +0000

    Added email management

--- config.py
+++ config.py
@@ -3,3 +3,12 @@ DEBUG=False # turn off debug mode
 SQLALCHEMY_ECHO=False # Turn off sqlalchemy echo of commands
 SQLALCHEMY_TRACK_MODIFICATIONS=False # Turn off sqlalchemy track modification in production enviroment
 BCRYPT_LEVEL=12 # Bcrypt config parameter
+
+# Email Configuration
+MAIL_SERVER=''
+MAIL_HOSTNAME=''
+MAIL_PORT=25
+MAIL_USE_TLS=False
+MAIL_USE_SSL=False
+MAIL_USERNAME=''
+MAIL_PASSWORD=''
---
+++ pa_web/emails.py
@@ -0,0 +1,11 @@
+# Email management
+from flask_mail import Message
+from pa_web import mail
+
+# Send email
+def send_email(subject, sender, to, text_body, html_body):
+    msg = Message(subject, sender=sender, recipients=[to])
+    msg.body = text_body
+    msg.html = html_body
+    mail.send(msg)
+
