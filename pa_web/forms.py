908b8aec8c9cb4326a006614619ca3221f4d9be8
ilbidi <fbidinotto@gmail.com>
2017-08-26 12:22:09 +0200

    Added contact form

--- pa_web/forms.py
+++ pa_web/forms.py
@@ -1,9 +1,15 @@
 # Forms definition
 from flask_wtf import Form
-from wtforms import StringField, PasswordField, SubmitField
+from wtforms import StringField, PasswordField, SubmitField, TextAreaField
 from wtforms.validators import DataRequired, Email
 
 class EmailPasswordForm(Form):
     email = StringField('Email', validators=[DataRequired(), Email()])
     password = PasswordField('Password', validators=[DataRequired()])
     submit = SubmitField('Log in')
+
+# Class contacts, send and email
+class ContactsForm(Form):
+    email = StringField('Email', validators=[DataRequired(), Email()])
+    request = TextAreaField('Request')
+    submit = SubmitField('Contact us.')
