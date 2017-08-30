5378d9e0607dd9c8acaa17f4adad3ab7468bfcc1
ilbidi <fbidinotto@gmail.com>
2017-08-31 00:35:29 +0200

    Commit all

--- pa_web/emails.py
+++ pa_web/emails.py
@@ -4,8 +4,8 @@ from pa_web import mail
 
 # Send email
 def send_email(subject, sender, to, text_body, html_body):
+    """Send and email to one email address"""
     msg = Message(subject, sender=sender, recipients=[to])
     msg.body = text_body
     msg.html = html_body
     mail.send(msg)
-
