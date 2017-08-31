853b09f5d9b8ba862aafd34d106cee67a8f10143
ilbidi <fbidinotto@gmail.com>
2017-08-31 22:03:33 +0200

    Add password hashing to user model

--- pa_web/models.py
+++ pa_web/models.py
@@ -1,5 +1,6 @@
 # Models
 from . import db
+from werkzeug.security import generate_password_hash, check_password_hash
 
 class Role(db.Model):
     __tablename__ = 'role'
@@ -7,7 +8,7 @@ class Role(db.Model):
     name = db.Column(db.String(64), unique=True)
 
     users = db.relationship('User', backref='role', lazy='dynamic')
-    
+
     def __repr__(self):
         return '<Role %r>' % self.name
 
@@ -15,9 +16,20 @@ class User(db.Model):
     __tablename__ = 'user'
     id = db.Column(db.Integer, primary_key = True)
     username = db.Column(db.String(64), unique=True, index=True)
+    password_hash = db.Column(db.String(128))
 
     role_id = db.Column(db.Integer, db.ForeignKey('role.id'))
-    
+
+    @property
+    def pasword(self):
+        raise AttributeError('Password is not a readable attribute')
+
+    @password.setter
+    def password(self, password):
+        self.password_hash = generate_password_hash(password)
+
+    def verify_password(self, password):
+        return check_password_hash(self.password_hash, password)
+
     def __repr__(self):
         return '<User %r>' % self.username
-    
---
+++ tests/test_user_model.py
@@ -0,0 +1,11 @@
+import unittest
+from pa_web.models import User
+
+class UserModelTestCase(unittest.TestCase):
+    def test_password_setter(self):
+        user = User( username = 'test', password='test1')
+        self.assertTrue(user..password_hash is not None)
+
+    def test_no_password_getter(self):
+        user = User( username = 'test', password='test1')
+        with self.assertRaises(AttributeError)
