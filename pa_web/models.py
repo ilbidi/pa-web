b9bad39df3cd1ce1e69ddfe5ba832d8165402f93
ilbidi <fbidnotto@gmail.com>
2017-09-14 21:09:06 +0000

    Started role management

--- pa_web/models.py
+++ pa_web/models.py
@@ -9,12 +9,45 @@ class Role(db.Model):
     __tablename__ = 'role'
     id = db.Column(db.Integer, primary_key = True)
     name = db.Column(db.String(64), unique=True)
-
+    deafult = db.Column(db.Boolean, default=False, index=True)
+    permission = db.Column(db.Integer,)
+    
     users = db.relationship('User', backref='role', lazy='dynamic')
 
     def __repr__(self):
         return '<Role %r>' % self.name
-
+    # Insert roles in db
+    @staticmethod
+    def insert_roles():
+        roles = {
+            'User' : (Permission.FOLLOW |
+                      Permission.COMMENT |
+                      Permission.WRITE_POST |
+                      , True), 
+            'SuperUser' : (Permission.FOLLOW |
+                           Permission.COMMENT |
+                           Permission.WRITE_POST |
+                           , False),
+            'Administrator' : ( 0xff, False)
+        }
+        for( r in roles ):
+            role = Role.query.filter_by(name=r).first()
+            if( role is None ):
+                role = Role.(name=r)
+            role.permission = roles[r][0]
+            role.default = roles[r][1]
+            db.session.add(role)
+        db.session.commit()
+        
+    
+class Permission:
+    FOLLOW = 0x01
+    COMMENT = 0x02
+    WRITE_POST = 0x03
+    SUPER_USER = 0x04
+    ADMINISTRATOR = 0x80
+    
+    
 class User(UserMixin, db.Model):
     __tablename__ = 'user'
     id = db.Column(db.Integer, primary_key = True)
@@ -25,6 +58,15 @@ class User(UserMixin, db.Model):
 
     role_id = db.Column(db.Integer, db.ForeignKey('role.id'))
 
+    # Constructor
+    def __init__(self, **kwargs):
+        super(User, self).__init__(**kwargs)
+        if( self.role is None):
+            if( self.email == current_app,config('PAWEB_ADMIN') ):
+                self.role = Role.query.filter_by(permission=0xff).first()
+            else:
+                slef.role = Role.query.filter_by(default=True).first()
+        
     @property
     def password(self):
         raise AttributeError('Password is not a readable attribute')
@@ -46,6 +88,7 @@ class User(UserMixin, db.Model):
             return False
         self.confirmed = True
         db.session.add(self)
+        db.session.commit()
         return True
 
     def __repr__(self):
